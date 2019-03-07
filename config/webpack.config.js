5c61190b1bbe883631a7b9f68adf3e9c336e1b03
horn7 <mariusz.gudyn@gmail.com>
2019-03-07 16:15:18 +0100

    07-03 test

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -395,7 +395,9 @@ module.exports = function(webpackEnv) {
                 importLoaders: 1,
                 sourceMap: isEnvProduction
                   ? shouldUseSourceMap
-                  : isEnvDevelopment
+                  : isEnvDevelopment,
+                  modules: true,
+                  localIdentName: '[name]__[local]__[hash:base64:5]'
               }),
               // Don't consider CSS imports dead code even if the
               // containing package claims to have no side effects.
--- src/containers/Burgerbuilder/BurgerBuilder.js
+++ src/containers/Burgerbuilder/BurgerBuilder.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
+import Aux from '../../hoc/auxiliary'
 
 class BurgerBuilder extends Component {
-
+    render() {
+        return(
+            <Aux>
+                <div>Burger</div>
+                <div>BuilBurger</div>
+            </Aux>
+        )
+    }
 }
 
 export default BurgerBuilder;
